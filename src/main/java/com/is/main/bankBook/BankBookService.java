9e5bc76003c7f1a02cfdf01aa821fedd755b729d
Hwangyunje <dnjy1945@naver.com>
2023-07-21 11:07:21 +0900

    0721

--- src/main/java/com/is/main/bankBook/BankBookService.java
+++ src/main/java/com/is/main/bankBook/BankBookService.java
@@ -1,5 +1,7 @@
 package com.is.main.bankBook;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,8 +11,12 @@ public class BankBookService {
 	@Autowired
 	private BankBookDAO bankBookDAO;
 	
+	public List<BankBookDTO> getList() throws Exception{
+		return bankBookDAO.getList();
+	}
+	
 	public BankBookDTO getDetail(BankBookDTO bankBookDTO)throws Exception{
-		return bankBookDTO.getDetail
+		return bankBookDTO.getDetail(bankbookDTO);
 	}
 	}
 	
